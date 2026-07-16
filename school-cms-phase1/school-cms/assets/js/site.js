50b82db7cd3ccc5fcccc6718cf9aa45e475cf149
Chetsada-creator <chetsada@wtp.ac.th>
2026-07-16 20:42:50 +0700

    Delete school-cms-phase1 directory

--- school-cms-phase1/school-cms/assets/js/site.js
+++
@@ -1,6 +0,0 @@
-function toggleTheme() {
-  const html = document.documentElement;
-  const next = html.dataset.theme === 'dark' ? 'light' : 'dark';
-  html.dataset.theme = next;
-  localStorage.setItem('theme', next);
-}
